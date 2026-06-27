6093e1cc624332f04397e4576890a6ec4865396b
ErikVlad <erikvlad69@mail.ru>
2026-06-27 19:32:59 +0600

    task05

---
+++ task05/ClassAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace task05;
+
+public class ClassAnalyzer
+{
+    private Type _type;
+
+    public ClassAnalyzer(Type type)
+    {
+        _type = type;
+    }
+
+    public IEnumerable<string> GetPublicMethods()
+    {
+        return _type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
+                    .Select(method => method.Name);
+    }
+
+    public IEnumerable<string> GetMethodParams(string methodname)
+    {
+        MethodInfo method = _type.GetMethod(methodname);
+        if (method == null)
+        {
+            return Enumerable.Empty<string>();
+        }
+        return method.GetParameters()
+                     .Select(param => param.Name);
+    }
+
+    public IEnumerable<string> GetAllFields()
+    {
+        return _type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static)
+                    .Select(field => field.Name);
+    }
+
+    public IEnumerable<string> GetProperties()
+    {
+        return _type.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static)
+                    .Select(prop => prop.Name);
+    }
+
+    public bool HasAttribute<T>() where T : Attribute
+    {
+        return _type.GetCustomAttribute<T>() != null;
+    }
+}
---
+++ task05tests/ClassAnalyzerTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using task05;
+
+namespace task05tests;
+
+public class TestClass
+{
+    public int PublicField;
+    private string _privateField;
+    public int Property { get; set; }
+
+    public void Method() { }
+    public void MethodWithParams(int id, string name) { }
+}
+
+[Serializable]
+public class AttributedClass { }
+
+public class ClassAnalyzerTests
+{
+    [Fact]
+    public void GetPublicMethods_ReturnsCorrectMethods()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var methods = analyzer.GetPublicMethods();
+
+        Assert.Contains("Method", methods);
+    }
+
+    [Fact]
+    public void GetAllFields_IncludesPrivateFields()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var fields = analyzer.GetAllFields();
+
+        Assert.Contains("_privateField", fields);
+    }
+
+    [Fact]
+    public void GetProperties_ReturnsCorrectProperties()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var properties = analyzer.GetProperties();
+
+        Assert.Contains("Property", properties);
+    }
+
+    [Fact]
+    public void HasAttribute_ReturnsTrueIfAttributeExists()
+    {
+        var analyzer = new ClassAnalyzer(typeof(AttributedClass));
+        var hasAttribute = analyzer.HasAttribute<SerializableAttribute>();
+
+        Assert.True(hasAttribute);
+    }
+
+    [Fact]
+    public void GetMethodParams_ReturnsCorrectParameterNames()
+    {
+        var analyzer = new ClassAnalyzer(typeof(TestClass));
+        var parameters = analyzer.GetMethodParams("MethodWithParams").ToList();
+
+        Assert.Contains("id", parameters);
+        Assert.Contains("name", parameters);
+    }
+}
