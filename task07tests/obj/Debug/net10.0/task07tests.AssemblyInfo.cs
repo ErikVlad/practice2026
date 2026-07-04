6515d7d1ca288230ec146fd670a7bb83d1f874cc
ErikVlad <erikvlad69@mail.ru>
2026-07-04 14:38:06 +0600

    task09

--- task07tests/obj/Debug/net10.0/task07tests.AssemblyInfo.cs
+++ task07tests/obj/Debug/net10.0/task07tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task07tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6093e1cc624332f04397e4576890a6ec4865396b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f08390fa67dad7de790b8cff8bc03a156529c7c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("task07tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task07tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
